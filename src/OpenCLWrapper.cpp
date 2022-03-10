6188d2cc5ba308a97b8a770b0b62312c9a5c23a1
Abubakr Nada <abubakr_ehab@hotmail.com>
2022-03-10 21:12:36 +0100

    updates

--- src/OpenCLWrapper.cpp
+++ src/OpenCLWrapper.cpp
@@ -1,7 +1,5 @@
 #include "include\OpenCLWrapper.h"
-#include "include\OpenCLWrapper.h"
-#include "include\OpenCLWrapper.h"
-#include "include\OpenCLWrapper.h"
+#include <fstream>
 void OpenCLProgram::FetchPlatforms() {
 
 	if (cl::Platform::get(&m_Platforms) != CL_SUCCESS) {
@@ -28,6 +26,21 @@ void OpenCLProgram::CreateContextsAndQueues()
 	}
 }
 
+std::vector<std::string>& OpenCLProgram::GetKernelsFromSource(const std::string& file_path)
+{
+	std::vector<std::string> kernel_names; 
+	std::ifstream file(file_path); 
+	for (std::string line; std::getline(file, line); ) {
+		if (line.find("__kernel") != std::string::npos) {
+			// found kernel 
+			std::size_t pos = line.find(' ', 0); 
+			std::size_t pos2 = line.find(' ', pos + 1); 
+			std::string kernel_name = line.substr(pos+1, pos2-pos-1 ); 
+			kernel_names.push_back(kernel_name); 
+		}
+	}
+}
+
 void OpenCLProgram::Init()
 {
 	FetchPlatforms();
@@ -46,8 +59,15 @@ void OpenCLProgram::AddBuffer(const std::string& buffer_name, const std::vector<
 
 }
 
-void OpenCLProgram::CreateKernels(const std::string& file_path)
+// With linking 
+void OpenCLProgram::CreateKernels(const std::string& file_path, const cl::Context& context)
 {
+	// create program 
+	m_Programs.push_back(cl::Program(context, file_path, true)); 
+	// create kernels from string
+	for (auto& kernel_name : GetKernelsFromSource(file_path)) {
+		m_Kernels[kernel_name].kernel = cl::Kernel(m_Programs[m_Programs.size() - 1], kernel_name.c_str()); 
+	}
 
 }
 
--- src/include/OpenCLWrapper.h
+++ src/include/OpenCLWrapper.h
@@ -4,14 +4,9 @@
 #include <unordered_map>
 #include "CL/opencl.hpp" // using the cpp bindings
 #include "Core.h"
-class BNN_LIB clProgram {
-	cl::Buffer m_Buffers;
-
-};
 class BNN_LIB OpenCLProgram {
 	struct clBuffer {
-		std::vector<cl::Kernel> m_Kernels;
-		cl::Buffer m_DeviceBuffer; // memory at Device Buffer  
+			cl::Buffer m_DeviceBuffer; // memory at Device Buffer  
 		std::vector<float> m_HostData; // Memory Buffer at host device 
 		int m_workdim[3]; 
 		clBuffer(int work_dim[3] , const std::vector<float>&  data, const cl::Context& context , bool READ_ONLY) {
@@ -24,24 +19,40 @@ class BNN_LIB OpenCLProgram {
 
 		clBuffer(clBuffer& buffer) {
 			m_HostData = buffer.m_HostData; 
-			m_Kernels = buffer.m_Kernels; 
 			for (int i = 0; i < 3; i++) m_workdim[i] = buffer.m_workdim[i]; 
 			m_DeviceBuffer = buffer.m_DeviceBuffer; 
 		}
-
+		cl::Buffer& GetDeviceBuffer() {
+			return m_DeviceBuffer; 
+		}
+		int* GetWorkDimensions() {
+			return m_workdim; 
+		}
+		
 	};	
+	struct clKernel {
+		cl::Kernel kernel;
+		cl::NDRange range; 
+		int status = 0; // finished or not 
+		};
+	struct clDevice {
+		cl::Platform platform; 
+		cl::Context context; 
+		cl::CommandQueue queue; 
+	};
 private: 
+
 	std::vector<cl::Context>  m_Contexts; 
 	std::vector<cl::Platform> m_Platforms; 
 	std::vector<cl::Device> m_Devices; 
 	std::unordered_map<std::string, clBuffer> m_Buffers;
 	std::vector<cl::CommandQueue> m_CommandQueues; 
-	
 	std::vector<cl::Program> m_Programs;  
-
+	std::unordered_map<std::string, clKernel> m_Kernels; 
 	void FetchPlatforms();
 	void FetchDevices(); 
 	void CreateContextsAndQueues(); 
+	std::vector<std::string>& GetKernelsFromSource(const std::string& file_path); 
 public: 
 
 	OpenCLProgram(); 
@@ -50,11 +61,28 @@ public:
 	void Init() ; 
 	
 	void AddBuffer(const std::string& buffer_name, const std::vector<float>& data, const cl::Context& context, int work_dimension[3] ,bool READ_ONLY = false); 
-	void DivideWorkItems(const std::string& buffer_name, int work_item_count);
-	void CreateKernels(const std::string& file_path); // must be .cl  file 
-
-	void AttatchKernelToBuffer(const std::string& kernel_path, const std::string& buffer_name); 
 	
-	void RunPrograms();
+	// Operations on kernels 
+	void CreateKernels(const std::string& file_path, const cl::Context& context); // must be .cl  file 
+	template <typename T, typename... Args> 
+	void SetKernelArgs(const std::string& kernel_name, T& value, int index =0 , Args... args) {  // in order; 
+		if (m_Kernels.find(kernel_name) != m_Kernels.end()) {
+			m_Kernels[kernel_name].kernel.setArg(index, value); 
+		}
+		SetKernelArgs(kernel_name, args, index = index + 1); 
+	}
+	void SetKernelWorkDim(const std::string& kernel_name, int work_dimension[3]) {
+		m_Kernels[kernel_name].range = cl::NDRange(work_dimension[0], work_dimension[1], work_dimension[2]); 
+	}
+	void ExecuteKernel_b(const std::string& kernel_name, cl::NDRange offset= cl::NullRange) {  // blocking for this case
+		if (m_Kernels.find(kernel_name) != m_Kernels.end()) {
+			std::vector<cl::Event> clevents{cl::Event()};
+			m_CommandQueues[0].enqueueNDRangeKernel(m_Kernels[kernel_name].kernel, offset, m_Kernels[kernel_name].range, cl::NullRange, NULL, &clevents[0]); 
+			clevents[0].wait(); 
+		}
+	}
+	cl::Buffer &GetDeviceBuffer(const std::string& buffer_name) {
+		return m_Buffers[buffer_name].GetDeviceBuffer(); 
+	}
 	
 };
